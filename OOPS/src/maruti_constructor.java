2a91451db8d3c40167d6e9d387314f595d609a85
Marutiee <marutibalekundri2001@gmail.com>
2025-11-12 09:36:47 +0530

    Constructors in java

--- OOPS/src/maruti_constructor.java
+++ OOPS/src/maruti_constructor.java
@@ -1,18 +1,28 @@
 class student{
-    int id;
-    String name;
+    private int id;
+    private String name;
 
     student(int a,String b){
-        System.out.println("2 parameters");
+        id=a;
+        name=b;
     }
     student(int a,int b,int c){
         System.out.println("3 parameters");
     }
+
+    public int getid(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
 }
 public class maruti_constructor {
     public static void main(String[] args) {
-        student m=new student(8,6, 7);
+        student m=new student(2,"Chiku");
 
+        System.out.println("Id is "+m.getid());
+        System.out.println("Name is "+m.getName());
 
     }
 }
